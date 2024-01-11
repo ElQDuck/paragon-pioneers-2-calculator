8d96738a711edfac97c8992b5a23636448def40a
Eugen Selbstreich <qduck@mail.ru>
2024-01-11 18:59:31 +0100

    feat: add config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// For Github Pages: https://vitejs.dev/guide/static-deploy#github-pages
 export default defineConfig({
+  base: "/paragon-pioneers-2-calculator/",
   plugins: [react()],
 })
