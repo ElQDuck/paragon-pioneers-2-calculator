94398db0ee36f9e803e8e341ef7b44aa05e9c2c1
Eugen Selbstreich <qduck@mail.ru>
2024-01-14 01:32:43 +0100

    feat: minor changes

--- src/pages/production-chain/SettingsMenu.tsx
+++ src/pages/production-chain/SettingsMenu.tsx
@@ -1,10 +1,21 @@
 import Drawer from '@mui/material/Drawer'
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
 
 export const SettingsMenu = (props: { drawerOpen: boolean; setDrawerOpen: Function }) => {
+  const switchColor = "primary"
   return (
     <Drawer anchor="left" open={props.drawerOpen} onClose={() => props.setDrawerOpen(false)}>
-      <>Test 1</>
-      <>Test 2</>
+    <FormControlLabel
+        control={<Switch color={switchColor} />}
+        label="Expert Mode"
+        labelPlacement="start"
+      />
+      <FormControlLabel
+          control={<Switch color={switchColor} />}
+          label="Allow manual number input"
+          labelPlacement="start"
+        />
     </Drawer>
   )
 }
