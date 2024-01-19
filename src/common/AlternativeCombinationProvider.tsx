e0a438c0002e20b1201e8f661737e9bddb2e5ff4
Eugen Selbstreich <qduck@mail.ru>
2024-01-19 23:37:15 +0100

    feat: add info icon

--- src/common/AlternativeCombinationProvider.tsx
+++ src/common/AlternativeCombinationProvider.tsx
@@ -1,10 +1,27 @@
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined'
+import Box from '@mui/material/Box'
+import Tooltip from '@mui/material/Tooltip'
 import { globalExpertMode } from '../App'
 
 export const AlternativeCombinationProvider = (props: { combinationList: JSX.Element[] }) => {
-  return globalExpertMode.value
-    ? props.combinationList.map((building, index) => {
-        console.log(`${index} / ${props.combinationList.length}`)
+  return globalExpertMode.value ? (
+    props.combinationList.map((building, index) => {
+      if (index === props.combinationList.length - 1) {
         return building
-      })
-    : props.combinationList[0]
+      }
+      return (
+        <>
+          {building}
+          <>OR</>
+        </>
+      )
+    })
+  ) : (
+    <Box>
+      <Tooltip disableFocusListener title={"Enable 'Expert Mode' to see all possible combinations."}>
+        <InfoOutlinedIcon color="inherit" fontSize="small" sx={{ position: 'absolute' }} />
+      </Tooltip>
+      {props.combinationList[0]}
+    </Box>
+  )
 }
