7d1246a0cfc61dcf6f383230925f6dc129f59a6c
Eugen Selbstreich <qduck@mail.ru>
2024-01-20 22:51:38 +0100

    refactor: minor styling optimizations

--- src/common/AlternativeCombinationProvider.tsx
+++ src/common/AlternativeCombinationProvider.tsx
@@ -2,16 +2,17 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import Accordion from '@mui/material/Accordion'
 import AccordionDetails from '@mui/material/AccordionDetails'
 import AccordionSummary from '@mui/material/AccordionSummary'
-import { globalExpertMode } from '../App'
+import { globalExpertMode, globalInvertBuildingChainOrder } from '../App'
+import { ProviderPaperStyle } from '../assets/styling/BuildingStyle'
 
 export const AlternativeCombinationProvider = (props: { combinationList: JSX.Element[] }) => {
   return (
     <>
       <Accordion defaultExpanded={globalExpertMode.value}>
-        <AccordionSummary expandIcon={<ExpandMoreIcon />} >
-          {props.combinationList[0]}
-        </AccordionSummary>
-        <AccordionDetails>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>{props.combinationList[0]}</AccordionSummary>
+        <AccordionDetails
+          sx={{ ...ProviderPaperStyle, alignItems: globalInvertBuildingChainOrder.value ? 'end' : 'start' }}
+        >
           {props.combinationList.map((building, index) => {
             if (index !== 0) {
               return (
--- src/pages/production-chain/buildings/colonists/CopperSmelter.tsx
+++ src/pages/production-chain/buildings/colonists/CopperSmelter.tsx
@@ -7,7 +7,6 @@ import {
   BuildingImageSize,
   ConsumerPaperStyle,
   ProviderBoxStyle,
-  ProviderPaperStyle,
   SingleBuildingWithCount,
 } from '../../../../assets/styling/BuildingStyle'
 import { Arrow } from '../../../../common/Arrow'
@@ -18,6 +17,10 @@ import { LUMBERJACK_INFO, Lumberjack } from '../pioneers/Lumberjack'
 import { COPPER_MINE_INFO, CopperMine } from './CopperMine'
 
 import { globalInvertBuildingChainOrder } from '../../../../App'
+import { AlternativeCombinationProvider } from '../../../../common/AlternativeCombinationProvider'
+import { COPPER_MINE_TROPICAL_INFO, CopperMineTropical } from '../farmers/CopperMineTropical'
+import { CONIFER_LUMBERJACK_INFO, ConiferLumberjack } from '../northern-islands/ConiferLumberjack'
+import { COPPER_MINE_NORTH_INFO, CopperMineNorth } from '../northern-islands/CopperMineNorth'
 
 const ITERATION_TIME_IN_SECONDS = 480
 const PRODUCE_PER_ITERATION = 1
@@ -59,34 +62,54 @@ export const CopperSmelter = (props: { count: number }) => {
         </Box>
       </Paper>
       <Box sx={{ ...ProviderBoxStyle, alignItems: globalInvertBuildingChainOrder.value ? 'end' : 'start' }}>
-        <Paper
-          ref={providerRef1}
-          elevation={2}
-          sx={{ ...ProviderPaperStyle, alignItems: globalInvertBuildingChainOrder.value ? 'end' : 'start' }}
-        >
-          <Lumberjack
-            count={props.count * (COPPER_SMELTER_INFO.ConsumePerMinute.get('Wood')! / LUMBERJACK_INFO.ProducePerMinute)}
+        <Box ref={providerRef1}>
+          <AlternativeCombinationProvider
+            combinationList={[
+              <Lumberjack
+                count={
+                  props.count * (COPPER_SMELTER_INFO.ConsumePerMinute.get('Wood')! / LUMBERJACK_INFO.ProducePerMinute)
+                }
+              />,
+              <ConiferLumberjack
+                count={
+                  props.count *
+                  (COPPER_SMELTER_INFO.ConsumePerMinute.get('Wood')! / CONIFER_LUMBERJACK_INFO.ProducePerMinute)
+                }
+              />,
+            ]}
           />
-        </Paper>
+        </Box>
         AND
-        <Paper
-          ref={providerRef2}
-          elevation={2}
-          sx={{ ...ProviderPaperStyle, alignItems: globalInvertBuildingChainOrder.value ? 'end' : 'start' }}
-        >
-          <CopperMine
-            count={
-              props.count * (COPPER_SMELTER_INFO.ConsumePerMinute.get('Copper')! / COPPER_MINE_INFO.ProducePerMinute)
-            }
+        <Box ref={providerRef2}>
+          <AlternativeCombinationProvider
+            combinationList={[
+              <CopperMine
+                count={
+                  props.count *
+                  (COPPER_SMELTER_INFO.ConsumePerMinute.get('Copper')! / COPPER_MINE_INFO.ProducePerMinute)
+                }
+              />,
+              <DeepCopperMine
+                count={
+                  props.count *
+                  (COPPER_SMELTER_INFO.ConsumePerMinute.get('Copper')! / DEEP_COPPER_MINE_INFO.ProducePerMinute)
+                }
+              />,
+              <CopperMineTropical
+                count={
+                  props.count *
+                  (COPPER_SMELTER_INFO.ConsumePerMinute.get('Copper')! / COPPER_MINE_TROPICAL_INFO.ProducePerMinute)
+                }
+              />,
+              <CopperMineNorth
+                count={
+                  props.count *
+                  (COPPER_SMELTER_INFO.ConsumePerMinute.get('Copper')! / COPPER_MINE_NORTH_INFO.ProducePerMinute)
+                }
+              />,
+            ]}
           />
-          OR
-          <DeepCopperMine
-            count={
-              props.count *
-              (COPPER_SMELTER_INFO.ConsumePerMinute.get('Copper')! / DEEP_COPPER_MINE_INFO.ProducePerMinute)
-            }
-          />
-        </Paper>
+        </Box>
       </Box>
       <Arrow start={providerRef1} end={consumerRef} />
       <Arrow start={providerRef2} end={consumerRef} />
