9cf1f652809fa5b65fc6ec1a943ce254ca4e9e34
Eugen Selbstreich <qduck@mail.ru>
2024-02-17 22:56:21 +0100

    fix: naming

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/paragon-pioneers-2-calculator/",
   plugins: [react()],
+  esbuild: {
+    minifyIdentifiers: false, // Needed to keep the function names after the build as they are because they are used to display the building name on image hover
+  },
 })
